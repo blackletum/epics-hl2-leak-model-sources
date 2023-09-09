84f622ab2176aef206d94d52d6f9f3d02cfac651
The One Epicplayer <epicplayer400@gmail.com>
2023-09-09 15:07:14 +0100

    Added c17_male Models

---
+++ modelsrc/Humans/Bip01_Male/0compileallcitizens_ln2.bat
@@ -0,0 +1,15 @@
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" Male1.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" Male2.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" Male3.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" Male4.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" Male5.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" Male6.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" Male7.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" c17_Male1.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" c17_Male2.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" c17_Male3.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" c17_Male4.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" c17_Male5.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" c17_Male6.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/leaknet2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" c17_Male7.qc
+pause
