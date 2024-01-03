0dd25fecbd777673c1fad25f565de607c5b8c77d
The One Epicplayer <epicplayer400@gmail.com>
2024-01-03 00:34:32 +0000

    Added Wasteland Scanner Texture Remakes

---
+++ modelsrc/Wastelandscanner/0compileallscanners_ln2.bat
@@ -0,0 +1,4 @@
+"G:/otherstuff/Half.Life.2.Beta-ANON/anon-hl2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" wscanner_single/wscanner_single.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/anon-hl2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" wscanner_pair/wscanner_pair.qc
+"G:/otherstuff/Half.Life.2.Beta-ANON/anon-hl2/bin/studiomdl" -nop4 -game "G:/otherstuff/Half.Life.2.Beta-ANON/compilation/hl2/" wscanner_tri/wscanner_tri.qc
+pause
